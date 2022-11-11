826708a821b22417a4ae9a0ff4efdfa5c0b12e36
kadranb <kadrak02@hotmail.com>
2022-11-11 09:06:52 -0300

    ajuste req 2 e 7

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,11 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
-  const ModuleCategories = sequelize.define('Category', {
-    // id: DataTypes.NUMBER,
-    name: DataTypes.STRING,
-  }, {
-    modelName: 'Category',
-    underscored: true,
-    timestamps: false,
-  });
+  const ModuleCategories = sequelize.define(
+    'Category',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: DataTypes.STRING,
+    },
+    {
+      modelName: 'categorie',
+      underscored: true,
+      timestamps: false,
+    }
+  );
   return ModuleCategories;
 };
